43fbdec1a56e4c5bdb6c082cf21e7bba3580787a
Patrick Kissling <patrick.kissling@gmx.de>
2020-04-26 18:41:00 +0200

    Add nerd font

--- cask.sh
+++ cask.sh
@@ -3,10 +3,14 @@
 # update
 brew cask upgrade
 
+# taps
+brew tap homebrew/cask-fonts
+
 # cask
 brew cask install 1password
 brew cask install boostnote
 brew cask install docker
+brew cask install font-hack-nerd-font
 brew cask install fluor
 brew cask install google-chrome
 brew cask install hand-mirror
