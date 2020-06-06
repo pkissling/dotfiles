1c4ae7f5246bfaed7fb7ee9681bd33c5b7e0276d
Patrick Kissling <patrick.kissling@gmx.de>
2020-06-06 17:58:33 +0200

    Only load zsh if previously installed

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # install zsh, if not installed already
-# 2. check if ZSH folder actually exists
 if [ -z "${ZSH}" ] || ! [ -d "${ZSH}" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
   # set zsh as default shell
   chsh -s /bin/zsh
+
+  # load zsh
+  zsh
 fi
 
 # upgrade zsh
 sh "$ZSH/tools/upgrade.sh"
 
-# load zsh
-zsh
