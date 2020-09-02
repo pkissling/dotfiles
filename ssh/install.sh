ef1d866e8ceb7ce3e197d0d521e01ade338cb87a
Patrick Kissling <patrick.kissling@de.bosch.com>
2020-09-02 16:32:28 +0200

    Add work ssh key

--- ssh/install.sh
+++ ssh/install.sh
@@ -7,4 +7,11 @@ mkdir -p "${HOME}"/.ssh
 
 # create symlinks
 ln -sfv "${PWD}"/"${BASEDIR}"/private.pub "${HOME}"/.ssh
+ln -sfv "${PWD}"/"${BASEDIR}"/work.pub "${HOME}"/.ssh
 ln -sfv "${PWD}"/"${BASEDIR}"/config "${HOME}"/.ssh
+
+# create symlink to default id_rsa file, if not exists
+if [ ! -L "${HOME}"/.ssh/id_rsa.pub ] ; then
+    read -p 'Usage (work/private): ' USAGE
+    ln -sfv "${PWD}"/"${BASEDIR}"/"${USAGE}".pub "${HOME}"/.ssh/id_rsa.pub
+fi
