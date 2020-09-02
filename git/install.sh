2d3e7ca8f9620ac75cfeb51478321a41bb8ee0a8
Patrick Kissling <patrick.kissling@de.bosch.com>
2020-09-02 10:26:43 +0200

    Allow multiple git user configurations

--- git/install.sh
+++ git/install.sh
@@ -5,3 +5,9 @@ BASEDIR=$(dirname "$0")
 # create symlinks
 ln -sfv "${PWD}"/"${BASEDIR}"/.gitconfig "${HOME}"
 ln -sfv "${PWD}"/"${BASEDIR}"/.gitignore "${HOME}"
+
+# create symlink to .gitconfig_user, if not exists
+if [ ! -L "${HOME}"/.gitconfig_user ] ; then
+    read -p 'Usage (work/private): ' USAGE
+    ln -sfv "${PWD}"/"${BASEDIR}"/.gitconfig_"${USAGE}" "${HOME}"/.gitconfig_user
+fi
