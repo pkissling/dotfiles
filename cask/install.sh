68f0d4933cce7dc07322d0d21779c3d8c65a1f98
Patrick Kissling <patrick.kissling@de.bosch.com>
2020-09-21 09:08:58 +0200

    Fix cask install command

--- cask/install.sh
+++ cask/install.sh
@@ -9,7 +9,7 @@ brew upgrade --cask
 sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' "${BASEDIR}/packages.txt" |
     while read -r PACKAGE
     do
-        brew list --cask "${PACKAGE}" || brew install cask "${PACKAGE}"
+        brew list --cask "${PACKAGE}" || brew cask install "${PACKAGE}"
     done
 
 # cleanup
