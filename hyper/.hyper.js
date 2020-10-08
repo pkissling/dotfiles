520a98dc37bb7d45a8b763929792a712a8f342ef
Patrick Kissling <patrick.kissling@de.bosch.com>
2020-10-08 15:05:57 +0200

    Hide title, remove ligature support

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -148,14 +148,13 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hyper-active-tab',
-    'hyper-font-ligatures',
     'hyper-highlight-active-pane',
     'hyper-quit',
     'hyper-search',
     'hyper-solarized-patched',
     'hypercwd',
-    'hyperterm-cursor',
-    'hyperlinks'
+    'hyperlinks',
+    'hyper-hide-title'
   ],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
