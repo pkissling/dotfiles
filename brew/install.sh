cf34d7eba42b45423a44a587bd31021214fcb851
Patrick Kissling <patrick.kissling@gmx.de>
2022-03-05 15:06:00 +0100

    Create seperate Brewfiles based on usage

--- brew/install.sh
+++ brew/install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -ex
+USAGE=$(cat "${HOME}"/dotfiles/.profile)
 
 # install brew, if not installed
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install packages from Brewfile
-brew bundle --file "${HOME}"/dotfiles/brew/Brewfile
+brew bundle --file "${HOME}"/dotfiles/brew/Brewfile."${USAGE}"
 
-# update Brewfile
-brew bundle dump --file "${HOME}"/dotfiles/brew/Brewfile --force
+# create two temp files to compare delta
+cat "${HOME}"/dotfiles/brew/Brewfile > /tmp/Brewfile.concat && sed -e '1,2d' < "${HOME}"/dotfiles/brew/Brewfile."${USAGE}" >> /tmp/Brewfile.concat
+brew bundle dump --file /tmp/Brewfile.dump --force
+
+# identify delta (missing packages) and append to Brewfile
+grep --invert-match --file /tmp/Brewfile.concat /tmp/Brewfile.dump >> "${HOME}"/dotfiles/brew/Brewfile || true
