bf4982fe0706a331b3aa9f245bdc79ee32f1d6e9
Patrick Kissling <patrick.kissling@de.bosch.com>
2022-10-19 11:54:18 +0200

    Don't create symlink for vscode workspaces

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,7 +4,6 @@ set -ex
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/vscode/keybindings.json "${HOME}"/Library/Application\ Support/Code/User
 ln -sfv "${HOME}"/dotfiles/vscode/settings.json "${HOME}"/Library/Application\ Support/Code/User
-ln -sfv "${HOME}"/dotfiles/vscode/workspaces "${HOME}"/vscode-workspaces
 
 # compare list of installed extensions with extensions.txt and identify delta
 MISSING_EXTENSIONS=$(code --list-extensions | grep --ignore-case --invert-match --file /dev/stdin vscode/extensions.txt) || true
