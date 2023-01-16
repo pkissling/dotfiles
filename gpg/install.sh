4ea541e33d9de8f399fe63f9facf4d8891f26ef2
Patrick Kissling <patrick.kissling@de.bosch.com>
2023-01-16 11:37:13 +0100

    Create dedicated config files for gpg-agent
    To deal with different homebrew paths on different archs

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -ex
+USAGE=$(cat "${HOME}"/dotfiles/.profile)
 
 # create .gnupg folder, if not exist
 mkdir -p "${HOME}"/.gnupg
 
 # create symlink
-ln -sfv "${HOME}"/dotfiles/gpg/gpg-agent.conf "${HOME}"/.gnupg
+ln -sfv "${HOME}"/dotfiles/gpg/gpg-agent_"${USAGE}".conf "${HOME}"/.gnupg/gpg-agent.conf
 
 # import public gpg keys
 gpg --import ~/dotfiles/gpg/private.asc
