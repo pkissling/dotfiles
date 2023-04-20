6f795f822e4acf2348ea0adcfb3377c68c9b2432
Patrick Kissling <patrick.kissling@de.bosch.com>
2023-04-20 10:04:26 +0200

    Automatically sort Brewfiles

--- brew/install.sh
+++ brew/install.sh
@@ -15,8 +15,11 @@ brew upgrade
 brew bundle --file "${HOME}"/dotfiles/brew/Brewfile."${USAGE}"
 
 # create two temp files to compare delta
-cat "${HOME}"/dotfiles/brew/Brewfile > /tmp/Brewfile.concat && sed -e '1,3d' < "${HOME}"/dotfiles/brew/Brewfile."${USAGE}" >> /tmp/Brewfile.concat
+cat "${HOME}"/dotfiles/brew/Brewfile > /tmp/Brewfile.concat && cat "${HOME}"/dotfiles/brew/Brewfile."${USAGE}" >> /tmp/Brewfile.concat
 brew bundle dump --file /tmp/Brewfile.dump --force
 
 # identify delta (missing packages) and append to Brewfile
 grep --invert-match --file /tmp/Brewfile.concat /tmp/Brewfile.dump >> "${HOME}"/dotfiles/brew/Brewfile."${USAGE}" || true
+
+# sort Brewfiles
+find "${HOME}"/dotfiles/brew -type f -name "Brewfile*" ! -name "*.lock.json" -exec sort {} -o {} \;
