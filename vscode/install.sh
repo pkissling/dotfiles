8ec61454eca6c8431a8010c04a49e01c6f522384
Patrick Kissling <patrick.kissling@de.bosch.com>
2023-06-01 14:46:13 +0200

    Manage vscode extensions via Brewfile

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,13 +4,6 @@ set -ex
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/vscode/settings.json "${HOME}"/Library/Application\ Support/Code/User
 
-# compare list of installed extensions with extensions.txt and identify delta
-MISSING_EXTENSIONS=$(code --list-extensions | grep --ignore-case --invert-match --file /dev/stdin vscode/extensions.txt) || true
-for MISSING_EXTENSION in ${MISSING_EXTENSIONS}; do
-    code --install-extension "${MISSING_EXTENSION}"
-done
-code --list-extensions > "${HOME}"/dotfiles/vscode/extensions.txt
-
 # sort settings.json
 UUID=$(uuidgen)
 jq --sort-keys < "${HOME}"/dotfiles/vscode/settings.json > /tmp/"${UUID}"
