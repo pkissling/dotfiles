29054b122535745b5909dd9ae974dff5189e436c
Patrick Kissling <patrick.kissling@de.bosch.com>
2024-07-16 15:06:57 +0200

    Update Warp's settings.plist file automatically

--- warp/install.sh
+++ warp/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -ex
 
+# save previous config
+UUID=$(uuidgen)
+defaults read dev.warp.Warp-Stable > "/tmp/${UUID}"
+sed -i '' "s|/Users/$(whoami)|$\{HOME\}|g" "/tmp/${UUID}"
+
+# write net config to warp
 defaults write dev.warp.Warp-Stable -string "${HOME}"/dotfiles/warp/settings.plist
-defaults read dev.warp.Warp-Stable > "${HOME}"/dotfiles/warp/settings.plist
+DELTA=$(grep -vFxf "/tmp/${UUID}" "${HOME}"/dotfiles/warp/settings.plist || true)
+if [ -n "${DELTA}" ]; then
+  echo "${DELTA}" >> "${HOME}"/dotfiles/warp/settings.plist
+fi
