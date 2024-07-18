7d3c196bb15761390a4e0bd9132a2dc37c1ee6a7
Patrick Kissling <patrick.kissling@de.bosch.com>
2024-07-18 09:21:15 +0200

    Use mise config file instead of custom script to install

--- mise/install.sh
+++ mise/install.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
-while read -r TOOL; do
-  # if line ends with @default, run `mise use` instead of `mise install`
-  if [[ "${TOOL}" == *@default ]]; then
-    TOOL="${TOOL//@default/}"
-    mise use -g "${TOOL}"
-  else
-    mise install "${TOOL}"
-  fi
-done < mise/tools.txt
+# create mise folder, if not exist
+mkdir -p "${XDG_CONFIG_HOME}"/mise
 
-# sort tools.txt file
-UUID=$(uuidgen)
-sort "${HOME}"/dotfiles/mise/tools.txt > /tmp/"${UUID}"
-mv /tmp/"${UUID}" "${HOME}"/dotfiles/mise/tools.txt
+# create config symlink
+ln -sfv "${HOME}"/dotfiles/mise/config.toml "${XDG_CONFIG_HOME}"/mise/config.toml
+
+# update plugins
+mise plugin update
+
+# install tools
+mise install
