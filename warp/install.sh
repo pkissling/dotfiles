5d1ce7781d921469425d3858d3c9759ea55364a0
Patrick Kissling <patrick.kissling@de.bosch.com>
2024-08-06 08:40:25 +0200

    Reduce log output when installing warp

--- warp/install.sh
+++ warp/install.sh
@@ -9,6 +9,10 @@ sed -i '' "s|/Users/$(whoami)|$\{HOME\}|g" "/tmp/${UUID}"
 
 # write net config to warp
 defaults write dev.warp.Warp-Stable -string "${HOME}"/dotfiles/warp/settings.plist
+
+# dont log following generation of warp settings delta
+set +x
+
 DELTA=$(grep --fixed-strings --line-regexp --file /tmp/"${UUID}" "${HOME}"/dotfiles/warp/settings.plist | grep --invert-match --file "${HOME}"/dotfiles/warp/ignored_config_keys.txt)
 if [ -n "${DELTA}" ]; then
   echo "${DELTA}" > "${HOME}"/dotfiles/warp/settings.plist
