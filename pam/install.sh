f469c84a89fa1dffa0b488229a44f7de39ecc7f8
Patrick Kissling <clip_lye.0j@icloud.com>
2025-03-29 14:11:36 +0100

    Use artginzburg/sudo-touchid instead of custom script

--- pam/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# Allow sudo to use Touch ID
-if ! grep -q "pam_tid.so" "/etc/pam.d/sudo"; then
-    sudo sed -i '' '3i\
-auth       sufficient     pam_tid.so
-' "/etc/pam.d/sudo"
-fi
