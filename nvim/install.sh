ae1a56d40dac02a90bfc378d1f90f9a5ee6cd53b
Patrick Kissling <clip_lye.0j@icloud.com>
2025-03-29 14:14:22 +0100

    Use kickstart.nvim instead of custom nvim

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# create nvim folder, if not exist
-mkdir -p "${HOME}"/.config/nvim
+# create config folder, if not exist
+mkdir -p "${HOME}"/.config
 
 # create symlink
-ln -sfv "${HOME}"/dotfiles/nvim/init.vim "${HOME}"/.config/nvim
-
-# checkout vundle git repo, if not exist
-if [ ! -d "${HOME}"/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}"/.vim/bundle/Vundle.vim
-fi
-
-# install plugins
-nvim +PluginInstall +qall
-nvim +PlugUpdate +qall
+ln -sfv "${HOME}"/dotfiles/nvim/kickstart.nvim "${HOME}"/.config/nvim
