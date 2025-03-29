1b3e9d179abe18f9fa143d7c4a3ae5892f39c87e
Patrick Kissling <clip_lye.0j@icloud.com>
2025-03-29 15:22:07 +0100

    Add rust support for nvim

--- nvim/kickstart.nvim/init.lua
+++ nvim/kickstart.nvim/init.lua
@@ -996,6 +996,11 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
