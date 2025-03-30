3c2cc7659a4cd722984b2ab58dcb32f2467b3afa
Patrick Kissling <clip_lye.0j@icloud.com>
2025-03-30 16:13:59 +0200

    Add nvim-tree to nvim

--- nvim/kickstart.nvim/init.lua
+++ nvim/kickstart.nvim/init.lua
@@ -1001,6 +1001,15 @@ require('lazy').setup({
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    config = function()
+      require('nvim-tree').setup {}
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = '[E]xplorer' })
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
