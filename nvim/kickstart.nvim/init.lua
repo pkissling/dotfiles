d3f1d13437c7b461fd5f0b3999f0acd97a9a17c0
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-04-01 16:32:03 +0200

    Enable git-blame in nvim

--- nvim/kickstart.nvim/init.lua
+++ nvim/kickstart.nvim/init.lua
@@ -1010,6 +1010,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = '[E]xplorer' })
     end,
   },
+  {
+    'f-person/git-blame.nvim',
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
