ded25dd621a91c42afe939055f286c784cb44da8
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-04-02 12:51:10 +0200

    Fix nvim symlink creation

--- nvim/install.sh
+++ nvim/install.sh
@@ -4,5 +4,8 @@ set -e
 # create config folder, if not exist
 mkdir -p "${HOME}"/.config
 
+# remove existing nvim directory/symlink
+rm -rf "${HOME}/.config/nvim"
+
 # create symlink
 ln -sfv "${HOME}"/dotfiles/nvim/kickstart.nvim "${HOME}"/.config/nvim
