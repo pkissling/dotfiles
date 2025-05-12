f50ebbf544f7506963288cc0e3d4cdf425992f61
Patrick Kissling <clip_lye.0j@icloud.com>
2025-05-12 12:57:51 +0200

    Add mason

--- nvim/lua/plugins/replace-with-register.lua
+++ nvim/lua/plugins/replace-with-register.lua
@@ -2,6 +2,6 @@ return {
     "vim-scripts/ReplaceWithRegister",
     keys = {
         { "gr", "<Plug>ReplaceWithRegisterVisual", desc = "Replace with [R]egister" },
-        { "grr", "<Plug>ReplaceWithRegisterLine", desc = "Replace with [R]egister Line" }
+        { "gR", "<Plug>ReplaceWithRegisterLine", desc = "Replace with [R]egister Line" }
     }
 }
