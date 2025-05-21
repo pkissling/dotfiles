02d87fcc944dd096e860f0789fad49673e417139
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-05-21 18:21:29 +0200

    Create zed config directory if it does not exist

--- zed/install.sh
+++ zed/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ex
 
+# create config dir, if not exists
+mkdir -p "${HOME}/.config/zed"
+
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/zed/settings.json "${HOME}"/.config/zed/settings.json
