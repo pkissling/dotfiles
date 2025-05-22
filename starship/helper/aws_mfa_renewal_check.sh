3de25d4be85f40d8a8517bdb3b87562d87fe5ea5
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-05-22 11:29:45 +0200

    Refactor helper script to not use `gdate`

--- starship/helper/aws_mfa_renewal_check.sh
+++ starship/helper/aws_mfa_renewal_check.sh
@@ -2,18 +2,23 @@
 
 if [ "$(cat "${HOME}"/dotfiles/.profile)" != "work" ]; then
     # show icon only for work profile
-    exit 1;
+    exit 1
 fi
 
 CURRENT_EXPIRATION=$(cat "$HOME"/.aws/credentials.*-mfa-expiration)
-CURRENT_EXPIRATION_IN_SECONDS=$(gdate -d "$CURRENT_EXPIRATION" +%s)
-NOW_IN_SECONDS=$(gdate +%s)
+
+# Normalize date: replace +00:00 or +0000 with Z
+NORMALIZED_EXPIRATION=$(echo "$CURRENT_EXPIRATION" | sed -E 's/([+-]00:00|[+-]0000)$/Z/')
+
+# Parse CURRENT_EXPIRATION in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ
+CURRENT_EXPIRATION_IN_SECONDS=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" "$NORMALIZED_EXPIRATION" +%s 2>/dev/null)
+NOW_IN_SECONDS=$(date +%s)
 REMAINING_VALIDILITY_IN_MINUTES=$(( (CURRENT_EXPIRATION_IN_SECONDS - NOW_IN_SECONDS) / 60 ))
 
 if [ "${REMAINING_VALIDILITY_IN_MINUTES}" -ge 30 ]; then
     # credentials are still valid
-    exit 1;
+    exit 1
 else
     # credentials invalid
-    exit 0;
+    exit 0
 fi
