6d5eb63fad9548572d9b2d87fdb14aa4e322f060
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-05-23 09:51:04 +0200

    Enable flash nvim for default search

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,6 +1,16 @@
+-- luacheck: globals vim
+
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+        highlight = { backdrop = true }
+      }
+    }
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -8,8 +18,9 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-  config = function()
+  config = function(_, opts)
     local palette = require('gruvbox').palette
-    vim.api.nvim_command("hi FlashLabel guibg=" .. palette.faded_yellow .. " guifg=" .. palette.dark_red_soft)
+    vim.api.nvim_command("hi FlashLabel guibg=" .. palette.faded_yellow .. " guifg=" .. palette.dark_red_hard)
+    require("flash").setup(opts)
   end
 }
