6489c1db5c4507daa2da4ba9299e879cde8adc57
Patrick Kissling <clip_lye.0j@icloud.com>
2025-08-09 08:48:56 +0200

    Remove template leftover

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,11 +5,6 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require('nvim-treesitter.configs').setup {
-			ensure_installed = {
-				"lua",
-				"rust",
-				"vue"
-			},
 			sync_install = false,
 			auto_install = true,
 			ignore_install = { },
