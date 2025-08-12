3daaa806390307bd018fc99677e55140c5f7d97c
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-08-12 11:16:31 +0200

    Fix ghostty shaders symlink creation

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -3,7 +3,8 @@ set -ex
 
 # ensure config directory exists
 mkdir -p "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty
+mkdir -p "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
 
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/ghostty/config "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/config
-ln -sfv "${HOME}"/dotfiles/ghostty/shaders "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
+ln -sfv "${HOME}"/dotfiles/ghostty/shaders/* "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
