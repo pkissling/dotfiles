00e23a5e2ea36f487e8e4dee5a8e183931e742eb
Patrick Kissling <clip_lye.0j@icloud.com>
2025-09-09 19:20:54 +0200

    Delete shaders symlink before recreating it

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -6,4 +6,5 @@ mkdir -p "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty
 
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/ghostty/config "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/config
+rm -f "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
 ln -sfv "${HOME}"/dotfiles/ghostty/shaders "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
