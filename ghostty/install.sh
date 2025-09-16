9c31a2b949c7491dc827345e0b3e7a38a1873dda
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-09-16 13:46:04 +0200

    Upgrade ghostty to 1.2.0

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -2,9 +2,9 @@
 set -ex
 
 # ensure config directory exists
-mkdir -p "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty
+mkdir -p "${HOME}"/.config/ghostty
 
 # create symlinks
-ln -sfv "${HOME}"/dotfiles/ghostty/config "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/config
-rm -f "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
-ln -sfv "${HOME}"/dotfiles/ghostty/shaders "${HOME}"/Library/Application\ Support/com.mitchellh.ghostty/shaders
+ln -sfv "${HOME}"/dotfiles/ghostty/config "${HOME}"/.config/ghostty/config
+rm -f "${HOME}"/.config/ghostty/shaders
+ln -sfv "${HOME}"/dotfiles/ghostty/shaders "${HOME}"/.config/ghostty/shaders
