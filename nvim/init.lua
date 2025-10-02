02f28f18a85f3f05fcb0a893e6dd2979fd729117
Patrick Kissling <patrick.kissling@de.bosch.com>
2025-10-02 16:49:59 +0200

    Auto update externally changed files in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,5 +39,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Auto update lazy.nvim plugins
 vim.api.nvim_create_autocmd("VimEnter", { callback = function() require("lazy").update({ show = false }) end })
 
+-- Auto reload file if changed externally
+vim.opt.autoread = true
+
 -- init lazy.nvim
 require("config.lazy")
