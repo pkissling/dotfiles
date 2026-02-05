de7b7d59faa9d6067dbdbad62cef337a6f160e9f
Patrick Kissling <clip_lye.0j@icloud.com>
2026-02-05 11:34:57 +0100

    Update treesitter plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,21 +3,22 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			sync_install = false,
-			auto_install = true,
-			ignore_install = { },
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			}
-		}
+	opts = {
+		auto_install = true,
+		highlight = { enable = true },
+	},
+	config = function(_, opts)
+		require('nvim-treesitter').setup(opts)
+
+		-- Disable highlighting for large files
+		vim.api.nvim_create_autocmd("BufReadPre", {
+			callback = function(args)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.uv.fs_stat, args.file)
+				if ok and stats and stats.size > max_filesize then
+					vim.treesitter.stop(args.buf)
+				end
+			end,
+		})
 	end
 }
