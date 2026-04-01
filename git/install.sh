efe03d785180a353bb4b7ba8a979e2c93ceb6d2f
Patrick Kissling <patrick.kissling@de.bosch.com>
2026-04-01 09:32:04 +0200

    Symlink global git config files under `~` instead of creating `~/.git` folder

--- git/install.sh
+++ git/install.sh
@@ -2,17 +2,14 @@
 set -ex
 USAGE=$(cat "${HOME}"/dotfiles/.profile 2>/dev/null || true)
 
-# create new git folder
-mkdir -p "${HOME}"/.git
-
 # create symlinks
 ln -sfv "${HOME}"/dotfiles/git/.gitconfig "${HOME}"
-ln -sfv "${HOME}"/dotfiles/git/.gitignore_global "${HOME}"/.git
-ln -sfv "${HOME}"/dotfiles/git/bin "${HOME}"/.git
+ln -sfv "${HOME}"/dotfiles/git/.gitignore_global "${HOME}"
+ln -sfv "${HOME}"/dotfiles/git/.git_functions "${HOME}"/.git_functions
 
 # create symlink for profile specific configuration, or empty file if USAGE is not set
 if [[ -n "${USAGE}" && -f "${HOME}/dotfiles/git/.gitconfig_${USAGE}" ]]; then
-	ln -sfv "${HOME}"/dotfiles/git/.gitconfig_"${USAGE}" "${HOME}"/.git/.gitconfig_user
+	ln -sfv "${HOME}"/dotfiles/git/.gitconfig_"${USAGE}" "${HOME}"/.gitconfig_user
 else
-	touch "${HOME}"/.git/.gitconfig_user
+	touch "${HOME}"/.gitconfig_user
 fi
