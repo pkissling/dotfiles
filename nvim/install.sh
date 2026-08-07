9e5a105ca765d5ac5fa7c179a60892c4fdc18379
Patrick Kissling <clip_lye.0j@icloud.com>
2026-08-07 09:34:17 +0200

    Discard local nvim plugin changes before sync

--- nvim/install.sh
+++ nvim/install.sh
@@ -10,5 +10,12 @@ rm -rf "${HOME}/.config/nvim"
 # create symlink
 ln -sfv "${HOME}"/dotfiles/nvim "${HOME}"/.config/nvim
 
+# discard local changes in plugin repos so lazy can update them
+for plugin in "${HOME}"/.local/share/nvim/lazy/*/; do
+    if [ -d "${plugin}/.git" ]; then
+        git -C "${plugin}" reset --hard --quiet
+    fi
+done
+
 # update plugins
 nvim --headless "+Lazy! sync" +qa
