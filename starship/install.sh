0efbc27c520d4d4d6e77ecd64718841ea05a0c17
Patrick Kissling <patrick.kissling@ebx.energy>
2026-09-01 17:46:26 +0200

    Setup work laptop

--- starship/install.sh
+++ starship/install.sh
@@ -7,5 +7,3 @@ mkdir -p "${HOME}"/.starship/helper
 
 # create symlinks for starship.toml config file
 ln -sfv "${HOME}"/dotfiles/starship/starship.toml "${HOME}"/.starship
-# create symlinks for helper scripts
-ln -sfv "${HOME}"/dotfiles/starship/helper/aws_mfa_renewal_check.sh "${HOME}"/.starship/helper
